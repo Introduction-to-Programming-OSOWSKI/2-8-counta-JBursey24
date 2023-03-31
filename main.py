efe9db35c1cfda1cc6920f6727659bc475571e5a
JBursey24 <817668@mystma.org>
2023-03-31 14:18:20 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(word):
+    numA=0
+    for i in range(0,len(word)):
+        if word[i]=="a":
+            numA = numA+1
+    return "There are " + str(numA)+ "a's in the word "+ word
